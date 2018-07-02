f7c7ce0dd1dae7a060c1fdf73b9b98f80d2a11c2
Nathen Watters <nwatters@homeadvisor.com>
2018-07-01 19:03:25 -0600

    btc pairing rate api test updates

--- test/api/shape_shift/rate.spec.js
+++ test/api/shape_shift/rate.spec.js
@@ -1,6 +1,7 @@
 import { default as request } from 'supertest';
 import { expect } from 'chai';
 import { base } from '../../../lib/urls';
+import { btcPairings } from '../../../lib/known_pairings';
 
 describe('Rate API', function() {
 
@@ -13,6 +14,30 @@ describe('Rate API', function() {
         expect(response.body.pair).to.exist;
         expect(response.body.rate).to.exist;
       });
+  });
+
+  it('Pairing batman and ironman should yield an error message in the response body', function() {
+    return request(base)
+      .get('/rate/batman_ironman')
+      .then(response => {
+        expect(response.body).to.exist;
+        expect(response.body.error).to.exist;
+        expect(response.body.error.toLowerCase()).to.include('currently unavailable');
+      });
+  });
+
+  btcPairings.forEach(pair => {
+    it(`BTC paired with ${pair} should return a successful response`, function() {
+      return request(base)
+        .get(`/rate/btc_${pair}`)
+        .expect(200)
+        .then(response => {
+          expect(response.body).to.exist;
+          expect(response.body.error).to.not.exist;
+          expect(response.body.pair).to.exist;
+          expect(response.body.rate).to.exist;
+        });
+    });
   });
 
 });
