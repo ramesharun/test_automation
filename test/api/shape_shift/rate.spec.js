4d7ec75c24a39e075453ab459187ad2f7184bcf9
Nathen Watters <nwatters@homeadvisor.com>
2018-07-01 18:26:24 -0600

    smoke api test for rate api

--- test/api/shape_shift/rate.spec.js
+++ test/api/shape_shift/rate.spec.js
@@ -1,9 +1,18 @@
-import 'supertest';
+import { default as request } from 'supertest';
 import { expect } from 'chai';
-import { rate } from '../../../lib/urls';
+import { base } from '../../../lib/urls';
 
-describe('Pair Endpoint from Rate API', function() {
+describe('Rate API', function() {
 
-  it('')
+  it('Should get a successful response for BTC-LTC pairing', function() {
+    return request(base)
+      .get('/rate/btc_ltc')
+      .expect(200)
+      .then(response => {
+        expect(response.body).to.exist;
+        expect(response.body.pair).to.exist;
+        expect(response.body.rate).to.exist;
+      });
+  });
 
 });
