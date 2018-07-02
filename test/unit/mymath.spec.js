905010274a3f07b99e591c9299b3fb16b2fdd13c
Nathen Watters <nwatters@homeadvisor.com>
2018-07-02 05:54:02 -0600

    Unit tests for subtract, multiply, divide

--- test/unit/mymath.spec.js
+++ test/unit/mymath.spec.js
@@ -7,15 +7,15 @@ class MyMath {
   }
 
   subtract(a, b) {
-    return a + b;
+    return a - b;
   }
 
   multiply(a, b) {
-    return a / b;
+    return a * b;
   }
 
   divide(a, b) {
-    return a * b;
+    return a / b;
   }
 
 }
@@ -37,10 +37,31 @@ describe('MyMath Tests', function() {
     });
   });
 
-  describe('subtraction', function() {});
+  describe('subtraction', function() {
+    it('should subtract two positive numbers', function() {
+      expect(math.subtract(3, 1)).to.eql(2);
+    });
+    it('should subtract two negative numbers', function() {
+      expect(math.subtract(-2, -2)).to.eql(0);
+    });
+  });
 
-  describe('multiplication', function() {});
+  describe('multiplication', function() {
+    it('should multiply two positive numbers', function() {
+      expect(math.multiply(3, 5)).to.eql(15);
+    });
+    it('should multiple two negative numbers', function() {
+      expect(math.multiply(-2, -6)).to.eql(12);
+    });
+  });
 
-  describe('division', function() {});
+  describe('division', function() {
+    it('should divide two positive numbers', function() {
+      expect(math.divide(15, 3)).to.eql(5);
+    });
+    it('should divide two negative numbers', function() {
+      expect(math.divide(-20, -5)).to.eql(4);
+    });
+  });
 
 });
